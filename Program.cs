9a9240658ba2493b8a4c43da82e16d950bab1099
evann8226-cmd <evann8226@gmail.com>
2026-02-09 10:54:20 +0700

    akhir permainan

--- Program.cs
+++ Program.cs
@@ -4,17 +4,60 @@ wizard wizardB = new wizard("jefta", 30);
 
 Console.WriteLine("Permainan di mulai........\n");
 wizardA.ShowStats();
-wizardB.ShowStats();    
+wizardB.ShowStats();
 
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
-wizardA.heal();
-wizardA.Attack(wizardB);
+//wizardA.Attack(wizardB);
+//wizardB.Attack(wizardA);
+//wizardA.heal();
+//wizardA.Attack(wizardB);
+string pilihan;
+while (wizardA.Energy > 0 && wizardB.Energy > 0)
+{
+    Console.WriteLine($"1.  {wizardA.Name} menyerang {wizardB.Name}");
+    Console.WriteLine($"2.  {wizardB.Name} menyerang {wizardA.Name}");
+    Console.WriteLine($"3.  {wizardA.Name} melakukan heal");
+    Console.WriteLine($"4.  {wizardB.Name} melakukan heal");
+
+    Console.Write("\nmasukan pilihanmu (1/2/3/4/):  ");
+    pilihan = Console.ReadLine();
+
+    if ( pilihan == "1")
+    {
+        wizardA.Attack( wizardB );  
+    }
+    else if ( pilihan == "2")
+    {
+        wizardB.Attack( wizardA );
+    }
+    else if( pilihan == "3")
+    {
+        wizardA.heal();
+    }
+    else if (pilihan == "4")
+    {
+        wizardB.heal();
+    }
+    else
+    {
+        Console.WriteLine("pilihan tidak valid");
+    }
+
+        
+}
 
 Console.WriteLine("Permainan berakhir.....\n");
+Console.WriteLine("statistik akhir");
 wizardA.ShowStats();
 wizardB.ShowStats();
 
+if(wizardA.Energy < wizardB.Energy)
+{
+    Console.WriteLine($"{wizardA.Name} memenangkan duel!!");
+}
+else
+{
+    Console.WriteLine($"{wizardB.Name} memenangkan duel!!");
+}
 
 public class wizard
 {
@@ -27,7 +70,7 @@ public class wizard
     public wizard (string name, int damage)
     {
         Name = name;
-        Energy = 100;
+        Energy = 50;
         damage = damage;
 
     }
@@ -49,14 +92,14 @@ public class wizard
     public void heal()
     {
         Energy += 5;
-        if(Energy < 100)
+        if(Energy < 50)
         {
             Energy += 5;
             Console.WriteLine($"{Name} melakukan heal energi meningkatkan menjadi {Energy}");
         }
         else
         {
-            Energy = 100;
+            Energy = 50;
             Console.WriteLine("Energi sudah penuh");
         }
 
